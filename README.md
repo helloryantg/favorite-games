4e9f9fcdcebdee1b2627abc50afc4cc9337e7a2e
Ryan Gonzales <helloryantg@gmail.com>
2019-02-05 22:44:39 -0800

    install backend dependencies and mongoDB local database

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 * npm i node-sass
 * added semantic-ui to html document
 * npm i styled-components
+* npm i express morgan serve-favicon mongoose dotenv
+* add "proxy": "http://localhost:3001",
+* place undernearth scrips in package.json
+
+## 
+* to start, use nodemon server and then npm start
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+
+const app = express();
+
+// require('dotenv').config();
+require('./config/database');
+
+app.use(logger('dev'));
+app.use(express.json());
+
+// Serve-favicon & static middlewares to serve from production 'build' folder
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+// API routes here, before the "catch all" route
+
+// Catch all route for proper client-side routing
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+let port = process.env.PORT || 3001;
+
+app.listen(port, function() {
+    console.log(`Express app running on port ${port}`);
+});
